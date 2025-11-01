8617c1f1dc3163259655d7ce8c38a901545d328b
Abdullah Ahmed <abdullah.ahmed.pisces.1010@gmail.com>
2025-11-01 18:53:57 +0530

    understanding Structured

---
+++ OutputParsers/structured_outputparser.py
@@ -0,0 +1,30 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StructuredOutputParser, ResponseSchema
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(
+    model="models/gemini-2.5-flash"
+)
+
+schema = [
+    ResponseSchema(name="fact_1", description="Fact 1 about the topic"),
+    ResponseSchema(name="fact_2", description="Fact 2 about the topic"),
+    ResponseSchema(name="fact_3", description="Fact 3 about the topic"),
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template="Give me 3 facts about {topic}\n {format_instructions}",
+    input_variables=["topic"],
+    partial_variables={"format_instruction": parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({"topic": "black hole"})
+
+print(result)
