057e1cbf6110473cd9f645a5ed9c21f0c880db2c
Abdullah Ahmed <abdullah.ahmed.pisces.1010@gmail.com>
2025-11-01 23:59:15 +0530

    implemented simple chain

---
+++ CHAINS/simple_chain.py
@@ -0,0 +1,25 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(
+    model="models/gemini-2.5-flash"
+)
+
+prompt = PromptTemplate(
+    template="Generate 5 interesting facts about {topic}",
+    input_variables=["topic"]
+)
+
+parser = StrOutputParser()
+
+# LCEL - LangChain Expression Language
+chain = prompt | model | parser
+
+result = chain.invoke({"topic": "football"})
+print(result)
+
+chain.get_graph().print_ascii()
